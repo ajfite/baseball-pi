45840f5c819d678363fee53e4bc7551ff46f9137
AJ Fite <goldman60@gmail.com>
2016-05-22 18:59:35 -0700

    Easy update, install, and run script for rapid deployment on my pi

---
+++ InstallAndRun.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# This script exists to make my own building and deployment on my pi easier
+#
+# IT IS NOT DESIGNED TO BE A GENERAL PURPOSE SCRIPT
+# RUN AT YOUR OWN RISK
+echo "Updating git repository"
+git pull
+echo "Removing Build Directory"
+rm -rf build
+echo "Remaking Build Directory"
+mkdir build
+cd build
+echo "Running cmake"
+cmake ..
+echo "Making"
+make
+echo "Executing baseball-pi with root (sudo) privileges"
+sudo ./baseball-pi
