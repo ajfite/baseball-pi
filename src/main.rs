1657e485ef1c8a5b099e529cd9ba3af6a6d7b594
AJ Fite <me@ajfite.com>
2021-05-01 21:01:00 -0700

    Hello World LEDs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 // Test program to get the dotstar LEDs working in rust
 //
-
-const CLK_PIN = 17;
-const DATA_PIN = 4;
+extern crate spidev;
+use std::io;
+use std::io::prelude::*;
+use spidev::{Spidev, SpidevOptions, SpidevTransfer, SpiModeFlags};
 
 fn main() {
-    println("Running rust dotstar test");
+    println!("Running rust dotstar test");
 
-    
+    let mut spi = create_spi().unwrap();
+
+    spi.write(&[0x00, 0x00, 0x00, 0x00, 
+              0xFF, 0x00, 0xFF, 0xFF,
+              0xFF, 0x00, 0xFF, 0xFF,
+              0xFF, 0x00, 0xFF, 0xFF,
+              0xFF, 0x00, 0xFF, 0xFF,
+              0xFF, 0x00, 0xFF, 0xFF,
+              0xFF, 0xFF, 0xFF, 0xFF]); 
 }
 
-fn putValue(val:bool) {
-
+fn create_spi() -> io::Result<Spidev> {
+    let mut spi = Spidev::open("/dev/spidev1.0")?;
+    let options = SpidevOptions::new()
+        .bits_per_word(8)
+        .max_speed_hz(200_000)
+        .mode(SpiModeFlags::SPI_MODE_0)
+        .build();
+    spi.configure(&options)?;
+    Ok(spi)
 }
